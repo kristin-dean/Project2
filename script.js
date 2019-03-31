c9fb1d95eac0156f2a65fb41d730f6b9624e9b51
kristin-dean <47224694+kristin-dean@users.noreply.github.com>
2019-03-31 12:53:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+//******************************read data file*****************************//
+var gradesP = d3.json("data.json");
+
+//*****************************make promise********************************//
+gradesP.then(function(d)
+{
+  drawInitial(d,37);
+  updateChart(d,5);
+},
+function(err)
+{
+  console.log(err);
+});
