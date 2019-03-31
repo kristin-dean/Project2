caac450b95e0b879ab09b066aeb92d041ad0d3f7
kristin-dean <47224694+kristin-dean@users.noreply.github.com>
2019-03-31 14:19:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 
 
-//******************************read 
-file*****************************//
+//******************************read data file*****************************//
 var gradesP = d3.json("classData.json");
 
 //*****************************make promise********************************//
 gradesP.then(function(d)
 {
   console.log("Working");
-  firstFunction(d);
+  drawChart(d);
 },
 function(err)
 {
   console.log(err);
 });
-  
+
 var drawChart = function(d)
   {
     d[0].quizes.forEach(function(d) {d.type="quiz"});
@@ -25,17 +24,18 @@ var drawChart = function(d)
     list2 = finalG.concat(d[0].homework);
     list3 = list2.concat(d[0].quizes);
     allGrades = list3.concat(d[0].test);
+    allGrades.forEach(function(d) {d.percent=(d.grade / d.max)*100});
     console.log(allGrades);
-    
+
      var screen =
       {
-        width:widths,
-        height:heights
+        width:850,
+        height:500
       };
-      var svg = d3.select(tag)
+      var svg = d3.select("svg")
         .attr("width",screen.width)
         .attr("height",screen.height);
-        
+
       var margins =
       {
         top:10,
@@ -43,11 +43,11 @@ var drawChart = function(d)
         left:45,
         right:75
       };
-      
+
       var width = screen.width - margins.left - margins.right;
       var height = screen.height - margins.top - margins.bottom;
       var xScale = d3.scaleLinear()
-        .domain([0,20])
+        .domain([0,40])
         .range([0,width]);
       var yScale = d3.scaleLinear()
         .domain([0,100])
@@ -57,33 +57,53 @@ var drawChart = function(d)
           .attr("transform","translate("+margins.left+","+margins.top+")");
      var colors = d3.scaleOrdinal(d3.schemeAccent);
       var students = plotLand.selectAll("g")
-          .data(d)
+          .data(allGrades)
           .enter()
           .append("g")
-          .attr("fill",function(d){return colors(d.name)})
       students.selectAll("circle")
-          .data(function(d){return d.grades;})
+          .data(allGrades)
           .enter()
           .append("circle")
           .attr("cx",function(d,i)
           {
-            return xScale(i);
+            return xScale(d.day);
           })
-          .attr("cy",function(d){return yScale(d);})
-          .attr("r",10);
+          .attr("cy",function(d){return yScale(d.percent);})
+          .attr("r", function(d) {
+            if (d.type == "quiz") {
+              return 6;}
+            else if (d.type == "homework") {
+              return 6;}
+            else if (d.type == "test") {
+              return 12;}
+            else if (d.type == "final") {
+              return 15;}})
+          .attr("fill", function(d) {
+            if (d.type == "quiz") {
+              return "blue";}
+            else if (d.type == "homework") {
+              return "green";}
+            else if (d.type == "test") {
+              return "purple";}
+            else if (d.type == "final") {
+              return "red";}
+            });
+
           var xAxis  = d3.axisBottom(xScale);
+
         svg.append("g")
           .classed(xAxis,true)
           .call(xAxis)
           .attr("transform","translate("+margins.left+","
           +(margins.top+ height + 15)+")"
-        );5
+        );
+        /*
       var legend = svg.append("g")
         .classed("legend",true)
         .attr("transform","translate("+
         (width + margins.left) + "," + margins.top+")");
         var legendLines = legend.selectAll("g")
-            .data(d)
+            .data(allGrades)
             .enter()
             .append("g")
             .classed("legendLines",true)
@@ -100,14 +120,11 @@ var drawChart = function(d)
         legendLines.append("text")
               .attr("x",20)
               .attr("y",function(d,i){return i*20+10;})
-              .text(function(d){return d.name;});
+              .text(function(d){return d.name;});*/
       var yAxis  = d3.axisLeft(yScale);
         svg.append("g")
           .classed(yAxis,true)
           .call(yAxis)
           .attr("transform","translate("+(margins.left-20)+","
           + 5 +")");
-      } 
-  
-}
-
+      }
